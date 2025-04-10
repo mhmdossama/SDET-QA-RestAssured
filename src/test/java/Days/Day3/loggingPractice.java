18be58851ea63fa0865d267716a12be6cb2b47af
osama <mhdsheta@gmail.com>
2025-04-10 13:59:06 +0200

    Intial commit adding Day1, Day2, and Day3. Day1 is empty

---
+++ src/test/java/Days/Day3/loggingPractice.java
@@ -0,0 +1,26 @@
+package Days.Day3;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Logging {
+    //logging
+    //log().all() - logs all the details of the request and response
+    //log().uri() - logs the URI of the request
+    //log().body() - logs the body of the request
+    //log().headers() - logs the headers of the request
+    //log().cookies() - logs the cookies of the request
+    //log().status() - logs the status code of the response
+    //log().ifError() - logs the response only if there is an error
+
+    @Test
+    public void logging() {
+        String baseUrl = "https://reqres.in/api/users";
+        given()
+                .when()
+                .get(baseUrl)
+                .then()
+                .log().all();
+    }
+}
---
+++ src/test/java/Days/Day3/summary.md
@@ -0,0 +1,12 @@
+What is Path parameters and Query parameters?
+
+Path parameters are used to identify a specific resource in the URL path.
+They are part of the URL and are typically used to specify a unique identifier for a resource.
+For example, in the URL `https://api.example.com/users/123`, `123` is a path parameter that identifies a specific user.
+
+Query parameters are used to filter or modify the response of an API request.
+They are appended to the URL after a question mark (`?`),
+and are typically used to specify optional parameters for the request.
+For example, in the URL `https://api.example.com/users?age=25&sort=asc`, `age` and `sort` are query parameters
+ that filter the results based on age and sort order.
+
